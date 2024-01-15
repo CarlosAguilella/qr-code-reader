58a76a893ad8128a304179c3f8ee3146172da108
marc <marcgaya98@gmail.com>
2024-01-15 13:52:42 +0100

    app qr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+import jsQR from 'jsqr';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const imageData = new Image();
+        imageData.onload = () => {
+          const canvas = document.createElement('canvas');
+          canvas.width = imageData.width;
+          canvas.height = imageData.height;
+          const context = canvas.getContext('2d');
+          context.drawImage(imageData, 0, 0, imageData.width, imageData.height);
+          const imageDataArray = context.getImageData(0, 0, imageData.width, imageData.height);
+
+          const code = jsQR(imageDataArray.data, imageData.width, imageData.height);
+
+          if (code) {
+            setResult(code.data);
+          } else {
+            setResult("No se encontró ningún código QR en la imagen.");
+          }
+        };
+        imageData.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+
+      <div className="input-container">
+        <h2>Aquí debes introducir el QR</h2>
+        <label className="file-label">
+          <input type="file" accept=".png" onChange={handleFileChange} />
+        </label>
+      </div>
+
+      {result && (
+        <div className="result-container">
+          <h2>Su resultado:</h2>
+          <p>{result}</p>
+          <h2>Y su QR:</h2>
+          <QRCode value={result} />
+        </div>
+      )}
     </div>
   );
 }
