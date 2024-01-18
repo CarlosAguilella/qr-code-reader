9f2a84ea9db3da349e0ddd99c9271aa85b31d21a
marc <marcgaya98@gmail.com>
2024-01-18 09:36:42 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
 import QrReader from 'react-qr-scanner';
-import jsQR from 'jsqr';
 import './App.css';
 
 function App() {
   const [resultado, setResultado] = useState(null);
   const [grabando, setGrabando] = useState(true);
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        setGrabando(false);
-        const imageData = new Image();
-        imageData.onload = () => {
-          const canvas = document.createElement('canvas');
-          canvas.width = imageData.width;
-          canvas.height = imageData.height;
-          const context = canvas.getContext('2d');
-          context.drawImage(imageData, 0, 0, imageData.width, imageData.height);
-          const imageDataArray = context.getImageData(0, 0, imageData.width, imageData.height);
-          const code = jsQR(imageDataArray.data, imageData.width, imageData.height);
-          if (code) {
-            setResultado(code.data);
-          } else {
-            setResultado("No se encontró ningún código QR en la imagen.");
-          }
-        };
-        imageData.src = event.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
   const handleCameraScan = (data) => {
-    setResultado(data);
-    setGrabando(false);
+    if (data) {
+      setResultado(data);
+      setGrabando(false);
+    }
   };
 
   const volverAEmpezar = () => {
@@ -46,13 +20,6 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={grabando ? 'input-container' : 'desaparecer input-container'}>
-        <h2>Aquí debes introducir el QR</h2>
-        <label className="file-label">
-          <input type="file" accept="image/*" onChange={handleFileChange} />
-        </label>
-      </div>
-
       <div className={grabando ? 'input-container' : 'desaparecer input-container'}>
         <h2>Abre la cámara trasera</h2>
         <QrReader
