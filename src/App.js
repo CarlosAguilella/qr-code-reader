edee55f7f0f4be0ed7f7bda3201667ddd640578e
marc <marcgaya98@gmail.com>
2024-01-18 09:52:06 +0100

    .

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ import './App.css';
 function App() {
   const [resultado, setResultado] = useState(null);
   const [grabando, setGrabando] = useState(true);
-  const [facingMode, setFacingMode] = useState('environment'); // 'user' para cámara frontal, 'environment' para cámara trasera
 
   const handleCameraScan = (data) => {
     if (data) {
       setResultado(data.text);
       setGrabando(false);
     }
-  };
-
-  const toggleFacingMode = () => {
-    setFacingMode((prevMode) => (prevMode === 'environment' ? 'user' : 'environment'));
   };
 
   const volverAEmpezar = () => {
@@ -26,15 +21,14 @@ function App() {
   return (
     <div className="app-container">
       <div className={grabando ? 'input-container' : 'desaparecer input-container'}>
-        <h2>Abre la cámara</h2>
+        <h2>Abre la cámara trasera</h2>
         <QrReader
           delay={300}
           style={{ height: '100%', width: '100%' }}
           onError={(err) => console.error(err)}
           onScan={handleCameraScan}
-          facingMode={facingMode}
+          facingMode={'environment'}
         />
-        <button onClick={toggleFacingMode}>Cambiar Cámara</button>
       </div>
 
       {resultado && (
