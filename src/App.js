8cc65078ce1ce927aa98925074def592f959c099
marc <marcgaya98@gmail.com>
2024-01-18 12:55:08 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
 import React, { useState } from 'react';
-import QrReader from 'react-qr-reader';
-import './App.css';
+import { QrReader } from 'react-qr-reader';
 
-function App() {
-  const [resultado, setResultado] = useState(null);
-  const [grabando, setGrabando] = useState(true);
-
-  const handleCameraScan = (data) => {
-    if (data) {
-      setResultado(data.text);
-      setGrabando(false);
-    }
-  };
-
-  const volverAEmpezar = () => {
-    setGrabando(true);
-    setResultado(null);
-  };
+const Test = (props) => {
+  const [data, setData] = useState('No result');
 
   return (
-    <div className="app-container">
-      <div className={grabando ? 'input-container' : 'desaparecer input-container'}>
-        <h2>Abre la cámara trasera</h2>
-        <QrReader
-          delay={300}
-          style={{ height: '100%', width: '100%' }}
-          onError={(err) => console.error(err)}
-          onScan={handleCameraScan}
-          constraints={{
-            audio: true,
-            video: { facingMode: "environment" }
-          }}
-        />
-      </div>
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
 
-      {resultado && (
-        <div className="resultado-container">
-          <h2>Tu resultado:</h2>
-          <p>{resultado}</p>
-        </div>
-      )}
-
-      <button className={grabando ? 'desaparecer input-container' : 'input-container'} onClick={volverAEmpezar}>
-        Volver al inicio.
-      </button>
-    </div>
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
   );
-}
-
-export default App;
-
-// html5-qrcode
+};
