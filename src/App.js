de64af28976718324ea527b6e3c622edc0837a58
marc <marcgaya98@gmail.com>
2024-01-30 09:33:19 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import QrReader from 'react-qr-reader';
-import QRcode from "react-qr-code";
+import QRCode from "react-qr-code";
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
   const [file, setFile] = useState(null);
   const [imageSrc, setImageSrc] = useState(null);
   const [qrData, setQrData] = useState("");
+  const qrCodeRef = useRef();
 
   const previewStyle = { height: 240, width: 320 };
   const delay = 50;
@@ -41,6 +42,36 @@ function App() {
     setGrabando(true);
     setResultado(null);
   };
+
+  const downloadQRCode = () => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const qrCodeSvg = qrCodeRef.current.querySelector("svg");
+
+    const svgString = new XMLSerializer().serializeToString(qrCodeSvg);
+    const DOMURL = window.URL || window.webkitURL || window;
+    const img = new Image();
+    const svgBlob = new Blob([svgString], { type: "image/svg+xml;charset=utf-8" });
+    const url = DOMURL.createObjectURL(svgBlob);
+
+    img.onload = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      canvas.width = qrCodeSvg.getAttribute("width");
+      canvas.height = qrCodeSvg.getAttribute("height");
+      context.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
+
+      const pngUrl = canvas.toDataURL("image/png");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = "qr-code.png";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    };
+
+    img.src = url;
+  }
 
   return (
     <div className='app-container'>
@@ -95,16 +126,26 @@ function App() {
           }}
         />
         <div>
-          <QRcode
-            className={qrData.length === 0 ? 'desaparecer' : ''}
-            value={qrData}
-            size={qrSize}
-            onError={errorQr}
-            style={{ marginTop: "2em" }}
-            bgColor={colorFondo}
-            fgColor={colorTexto}
-          />
+          <div ref={qrCodeRef}>
+            <QRCode
+              className={qrData.length === 0 ? 'desaparecer' : ''}
+              value={qrData}
+              size={qrSize}
+              onError={errorQr}
+              bgColor={colorFondo}
+              fgColor={colorTexto}
+            />
+          </div>
         </div>
+      </div>
+      
+      <div>
+        <input
+          type='button'
+          className={qrData.length === 0 ? 'desaparecer' : ''}
+          value="Descargar QR"
+          onClick={downloadQRCode}
+        />
       </div>
     </div>
   );
