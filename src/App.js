24b0dd0df2a3ae4426ef807c4bfb39522822ca39
marc <marcgaya98@gmail.com>
2024-02-21 12:28:04 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useRef } from 'react';
 
 import CameraQr from './components/CameraQr';
-import GeneratorQr from './components/GeneratorQr';
-import UploadImage from './components/UploadImage';
-import CanvasDrawing from './components/CanvasDrawing';
-import WhiteBoard from './components/WhiteBoard';
 
 import './App.css';
 
@@ -13,10 +9,6 @@ function App() {
   return (
     <div className='app-container'>
       <CameraQr />
-      <UploadImage />
-      <GeneratorQr />
-      {/* <CanvasDrawing /> */}
-      {/* <WhiteBoard /> */}
     </div>
   );
 }
