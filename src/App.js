f5b79aa295ddf48e8e7bf0fb9ebabbb3dbbdf330
marc <marcgaya98@gmail.com>
2024-02-26 13:50:56 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 
+import Header from './components/Header';
 import CameraQr from './components/CameraQr';
 
 import './App.css';
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className='app-container'>
+      <Header />
       <CameraQr />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src="header.png" alt="header" />
+        </div>
+    );
+}
+
+export default Header;
