1e0613800e25ff1a9d252a8c1f520e483ed403cb
marc <marcgaya98@gmail.com>
2024-05-29 16:46:44 +0200

    .

--- src/components/formulario/FacilitiesForm.js
+++ src/components/formulario/FacilitiesForm.js
@@ -72,7 +72,8 @@ const Formulario = () => {
                                 <CarruselFutbol />
                                 <div className="flex-space-around">
                                     {/* <a href="https://maps.app.goo.gl/ijWD9V4c6ZfuRrzJ8"><p>Localización</p></a> */}
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.308504443491!2d-0.1234101!3d39.89229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005da157036cd%3A0x1ec4f846411a686c!2sInstalaciones%20deportivas%2C%20Alquer%C3%ADas%20del%20Ni%C3%B1o%20Perdido!5e1!3m2!1ses!2ses!4v1716890548968!5m2!1ses!2ses" width="60%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.308504443491!2d-0.1234101!3d39.89229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005da157036cd%3A0x1ec4f846411a686c!2sInstalaciones%20deportivas%2C%20Alquer%C3%ADas%20del%20Ni%C3%B1o%20Perdido!5e1!3m2!1ses!2ses!4v1716890548968!5m2!1ses!2ses"
+                                        width="60%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </Grid>
                             <Grid item xs={12}>
@@ -92,7 +93,8 @@ const Formulario = () => {
                                 <CarruselPiscina />
                                 <div className="flex-space-around">
                                     {/* <a href="https://maps.app.goo.gl/Rcr4KEzBX8LygNrc9"><p>Localización</p></a> */}
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.1360915045443!2d-0.1173165!3d39.89557689999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005d69a7a5683%3A0x2a32011efbb4a3b6!2sPiscina%20Municipal%20de%20les%20Alqueries!5e1!3m2!1ses!2ses!4v1716890971546!5m2!1ses!2ses" width="60%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.1360915045443!2d-0.1173165!3d39.89557689999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005d69a7a5683%3A0x2a32011efbb4a3b6!2sPiscina%20Municipal%20de%20les%20Alqueries!5e1!3m2!1ses!2ses!4v1716890971546!5m2!1ses!2ses"
+                                        width="60%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </Grid>
                             <Grid item xs={12}>
@@ -112,7 +114,8 @@ const Formulario = () => {
                             <Grid item xs={12}>
                                 <div className="flex-center">
                                     {/* <a href="https://maps.app.goo.gl/UKg3ZzWqMqMig8XD7"><p>Localización</p></a> */}
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d898.7846733348335!2d-0.1176509!3d39.8955273!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005d79eb78877%3A0xc39d2fdcce8249af!2sPolideportivo%20Municipal!5e1!3m2!1ses!2ses!4v1716891017715!5m2!1ses!2ses" width="60%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d898.7846733348335!2d-0.1176509!3d39.8955273!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6005d79eb78877%3A0xc39d2fdcce8249af!2sPolideportivo%20Municipal!5e1!3m2!1ses!2ses!4v1716891017715!5m2!1ses!2ses"
+                                        width="60%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </Grid>
                             <Grid item xs={4}>
